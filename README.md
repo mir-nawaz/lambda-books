56ea8d8c7b3aee454965e912c0f0091a9f4cf15d
Mir Nawaz <inbox.mirnawaz@gmail.com>
2020-04-14 20:03:01 +0500

    serverless offline example :smile:

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-## Serverless cloud lambda aws. 
+= DyanmoDB Offline Serverless CRUD Example
+
+This is the example for Developing AWS locally with Serverless offline plugins:
+
+Install Serverless framework 
+
+$ `npm install serverless -g`
+
+Check Serverless installation by executing
+
+$ `serverless --version`
+
+Install npm dependencies 
+
+$ `npm install`
+
+Install dynamodb locally
+
+$ `sls dynamodb install`
+
+Start offline serverless
+
+$ `sls offline start`
+
+Now let's setup dynamodb-admin: which gives a good GUI to view our DynamoDB tables
+
+$ `npm install dynamodb-admin -g`
+
+$ `export DYNAMO_ENDPOINT=http://localhost:8000`
+
+$ `dynamodb-admin`
+
+All API testing samples
+
+$ `curl -XGET http://localhost:3000/dev/hello`
+
+$ `curl -XPOST http://localhost:3000/dev/book --data '{ "bookName": "book 01", "authorName": "author 01" }'`
+
+$ `curl -XGET http://localhost:3000/dev/book`
+
+$ `curl -XGET http://localhost:3000/dev/book/a01a9d22-6214-43ba-85bd-ac3e1d4b05e6`
+
+$ `curl -XPUT http://localhost:3000/dev/book/a01a9d22-6214-43ba-85bd-ac3e1d4b05e6 --data '{ "bookName": "book update 01", "authorName": "author update 01" }'`
+
+$ `curl -XDELETE http://localhost:3000/dev/book/a01a9d22-6214-43ba-85bd-ac3e1d4b05e6`
+
+CLI 
+
+![DyanmoDB Offline Serverless CRUD](books.png)
--- handler.js
+++ handler.js
@@ -1,18 +1,29 @@
 'use strict';
 
-import AWS from 'aws-sdk';
-import { uuid, isUuid } from 'uuidv4';
+const AWS = require('aws-sdk');
+const { uuid } =  require('uuidv4');
 
-const tableName = process.env.TABLE_NAME;
-const db = new AWS.DynamoDB.DocumentClient();
+const tableName = process.env.BOOKS_TABLE;
+const dbEndpoint = process.env.DYNAMODB_ENDPOINT;
+const IS_OFFLINE = process.env.IS_OFFLINE;
+
+let options = {};
+
+if (IS_OFFLINE === 'true') {
+  options = {
+    region: 'localhost',
+    endpoint: dbEndpoint,
+  }
+}
+
+const db = new AWS.DynamoDB.DocumentClient(options);
 
 module.exports.hello = async event => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        message: 'Go Serverless v1.0! Your function executed successfully!'
       },
       null,
       2
@@ -25,22 +36,23 @@ module.exports.add = async event => {
   const body = JSON.parse(event.body);
 
   // skipping validation for time saving 
-
+  const id = uuid(); 
+  
   const book = {
-    uuid: uuid(),
-    name: body.name,
-    releaseDate: body.releaseDate,
+    bookID: id,
+    bookName: body.bookName,
+    releaseDate: new Date().getTime(),
     authorName: body.authorName
   };
-
-  let res = await db.put({ TableName: tableName, Item: book }).promise();
+  
+  await db.put({ TableName: tableName, Item: book }).promise();
 
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: 'Book saved successfully!',
-        input: event,
+        bookID: id
       },
       null,
       2
@@ -51,34 +63,35 @@ module.exports.add = async event => {
 
 module.exports.update = async event => {
   const id = event.pathParameters.id;
-  const body = JSON.parse(event.body);
-  const { name, releaseDate, authorName } = body;
 
   // skipping validation for time saving 
 
+  const body = JSON.parse(event.body);
+  const { bookName, authorName } = body;
+  const releaseDate = new Date().getTime();
+
+
   const params = {
-    Key: {
-      uuid: id
-    },
+    Key: { bookID: id },
     TableName: tableName,
-    ConditionExpression: 'attribute_exists(uuid)',
-    UpdateExpression: 'SET name = :name, releaseDate = :releaseDate, authorName = :authorName',
+    ConditionExpression: 'attribute_exists(bookID)',
+    UpdateExpression: 'SET bookName = :bookName, releaseDate = :releaseDate, authorName = :authorName',
     ExpressionAttributeValues: {
-      ':name': name,
+      ':bookName': bookName,
       ':releaseDate': releaseDate,
       ':authorName': authorName
     },
     ReturnValues: 'ALL_NEW'
   };
 
-  let res = await db.update(params).promise(); 
+  let result = await db.update(params).promise(); 
 
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: 'Book updated successfully!',
-        input: event,
+        book: result.Attributes
       },
       null,
       2
@@ -88,24 +101,23 @@ module.exports.update = async event => {
 };
 
 module.exports.delete = async event => {
-    // skipping validation for time saving 
+  
+  // skipping validation for time saving 
 
   const id = event.pathParameters.id;
   const params = {
-    Key: {
-      uuid: id
-    },
+    Key: { bookID: id },
     TableName: tableName
   };
 
-  let res = await db.delete(params).promise();
+  await db.delete(params).promise();
 
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: 'Book deleted successfully!',
-        input: event,
+        uuid: id
       },
       null,
       2
@@ -115,25 +127,24 @@ module.exports.delete = async event => {
 };
 
 module.exports.detail = async event => {
+
   // skipping validation for time saving 
 
   const id = event.pathParameters.id;
 
   const params = {
-    Key: {
-      uuid: id
-    },
+    Key: { bookID: id },
     TableName: tableName
   };
   
-  let res = await db.get(params).promise();
+  let results = await db.get(params).promise();
 
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: 'Book detail successfully!',
-        book: res
+        book: results.Item || {}
       },
       null,
       2
@@ -144,20 +155,14 @@ module.exports.detail = async event => {
 
 module.exports.list = async event => {
 
-  // skipping validation for time saving 
-
-  const params = {
-    TableName: tableName
-  };
-
-  let res = db.scan(params).promise();
+  let result = await db.scan({TableName: tableName}).promise();
 
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: 'Book list successfully!',
-        books: res
+        books: result.Items || []
       },
       null,
       2
